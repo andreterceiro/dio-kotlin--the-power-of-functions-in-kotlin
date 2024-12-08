ca0ed1aac490291a5be35e6dccb8f1e0544e644d
André de Paula Terceiro <andre@terceiro.com.br>
2024-12-08 13:30:34 -0300

    feat: updating comments

--- tests/let.kt
+++ tests/let.kt
@@ -1,9 +1,10 @@
 package tests
 
 fun main() {
-    var ret:String = "test".let {
+    var ret:String = "test".let({
         toUpperCase(it) // It receives the ret value as calls a function
-    }
+                        // You don't need to use return - the last instruction is returned
+    })
 
     print(ret)
 }
