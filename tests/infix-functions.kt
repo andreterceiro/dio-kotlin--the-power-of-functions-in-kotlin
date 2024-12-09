a8e21f8ce962680055b845a9ba51769d51524ce1
André de Paula Terceiro <andre@terceiro.com.br>
2024-12-09 10:58:36 -0300

    feat: testing an internal infix function 'to'

--- tests/infix-functions.kt
+++ tests/infix-functions.kt
@@ -4,5 +4,14 @@ infix fun Int.times(str: String) = str.repeat(this)
 
 fun main() {
     print(2.times("Bye "))
+    print(" -- ")
+
     print(2 times("Hi! ")) // Also works without a dot!
+    print(" -- ")
+
+    val pair = "Júlio" to "Enzo"
+    print(pair)
+    print(" -- ")
+
+    print(pair::class.simpleName)
 }
